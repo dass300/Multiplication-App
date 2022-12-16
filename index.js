6c7dff3ad44465e581b62b90a3ff4f0629c4c030
dass300 <das300@gmail.com>
2022-12-16 23:30:49 +0530

    modified

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const num1 = Math.ceil(Math.random()*10)
 const num2 = Math.ceil(Math.random()*10)
+// const num1 = Math.ceil(Math.random()*10)
+// const num2 = Math.ceil(Math.random()*10)
+// const num1 = Math.ceil(Math.random()*10)
+// const num2 = Math.ceil(Math.random()*10)
+// const num1 = Math.ceil(Math.random()*10)
+// const num2 = Math.ceil(Math.random()*10)
+// const num1 = Math.ceil(Math.random()*10)
+// const num2 = Math.ceil(Math.random()*10)
+// const num1 = Math.ceil(Math.random()*10)
+// const num2 = Math.ceil(Math.random()*10)
 
 const questionEl = document.getElementById('question')
 const formEl = document.getElementById("form")
 
 questionEl.innerText = `What is ${num1} multiply by ${num2}?`
+// questionEl.innerText = `What is ${num1} multiply by ${num2}?`
+// questionEl.innerText = `What is ${num1} multiply by ${num2}?`
+// questionEl.innerText = `What is ${num1} multiply by ${num2}?`
 
 const correctAns = num1 * num2
 
